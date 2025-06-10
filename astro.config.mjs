95aa83cdddfd3e842c9855d90284dc11f3e9d9ec
RomaisaDjoghlal <mr_djoghlal@esi.dz>
2025-06-10 19:35:25 +0200

    Build static site for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+ site: 'https://romaisadjoghlal.github.io/my-portfolio/',
   base: '/my-portfolio/',
-  outDir: "dist", 
+  
   // other config options...
 });
